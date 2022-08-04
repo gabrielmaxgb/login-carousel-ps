24f7f8b0f664ea45093f4bc48327662a632777f0
Gabriel Max <gmaxgomes@gmail.com>
2022-08-04 14:15:15 -0300

    home + login + not found

---
+++ src/app/pages/notFound/NotFound.js
@@ -0,0 +1,11 @@
+import { NotFoundContainer } from "./NotFoundStyles";
+
+function NotFound() {
+  return (
+    <NotFoundContainer>
+      Nothing here!
+    </NotFoundContainer>
+  );
+};
+
+export default NotFound;
---
+++ src/app/pages/notFound/NotFoundStyles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 100vh;
+`;
